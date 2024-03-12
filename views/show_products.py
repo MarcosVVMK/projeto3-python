85ca5fd037bb113701ff056a4b99552b6ff7768c
Marcos Matos <marcosvm000@gmail.com>
2024-03-12 18:33:37 -0300

    finish show products

--- views/show_products.py
+++ views/show_products.py
@@ -4,9 +4,17 @@ import database.database
 def show_products_screen():
     print("#### Todos os produtos cadastrados ####")
     products = database.database.get_products()
+    count = 1
     for product in products:
-        format_product(product)
+        format_product(product, count)
+        count = count + 1
 
-def format_product(product):
-    # fazer um template para exibir
-    print(product)
+def format_product(product, count):
+    print("________________________________________________________")
+    print("#### Produto cadastrado: " + str(count) + " #### ")
+    print("| Nome do produto: " + product[1])
+    print("| Preço do produto: R$ {:,.2f}".format(product[2]))
+    print("| Quantitdade do produto em estoque: " + str(product[3]))
+    print("| Código de Barras EAN: " + str(product[4]))
+    print("| Categoria do produto: " + str(product[5]))
+    print("________________________________________________________")
